cc8a40cfd70f6937e592fa4e80e4a628c3f7b255
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-21 19:43:16 -0700

    added kids kayak text

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,31 @@ document.addEventListener("mousemove",(event)=>{
       }
     }
   } else {
-    let rowCells = document.querySelectorAll("p")
+    let rowCells = document.querySelectorAll(".rates-right p")
     for (let i =0; i< rowCells.length; i++){
         rowCells[i].classList.remove("hover-right")
+    }
+  }
+})
+
+
+document.addEventListener("mousemove",(event)=>{
+  console.log("nice")
+  if (event.target.closest(".rates-left p")) {
+    console.log("yeah")
+    let rowCells = document.querySelectorAll(".rates-left p")
+    console.log(rowCells);
+    for (let i =0; i< rowCells.length; i++){
+      if (rowCells[i].id !== "" && rowCells[i].id === event.target.closest("p").id ){
+        rowCells[i].classList.add("hover-left")
+      } else {
+        rowCells[i].classList.remove("hover-left")
+      }
+    }
+  } else {
+    let rowCells = document.querySelectorAll(".rates-left p")
+    for (let i =0; i< rowCells.length; i++){
+        rowCells[i].classList.remove("hover-left")
     }
   }
 })
