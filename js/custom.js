8948e386a74eb9e71b995a96c12855dbf611cda5
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-21 19:25:30 -0700

    removed more junk

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 document.addEventListener("mousemove",(event)=>{
   if (event.target.closest(".rates-right p")) {
     let rowCells = document.querySelectorAll(".rates-right p")
-    let rowCellsLeft = document.querySelectorAll(".rates-left p")
     for (let i =0; i< rowCells.length; i++){
       if (rowCells[i].id !== "" && rowCells[i].id === event.target.closest("p").id ){
         rowCells[i].classList.add("hover-right")
