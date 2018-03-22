3ebfed0ca7c9486ddde413e230248b769dac05fc
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-21 19:24:43 -0700

    renamed a class

--- js/custom.js
+++ js/custom.js
@@ -4,15 +4,15 @@ document.addEventListener("mousemove",(event)=>{
     let rowCellsLeft = document.querySelectorAll(".rates-left p")
     for (let i =0; i< rowCells.length; i++){
       if (rowCells[i].id !== "" && rowCells[i].id === event.target.closest("p").id ){
-        rowCells[i].classList.add("hover")
+        rowCells[i].classList.add("hover-right")
       } else {
-        rowCells[i].classList.remove("hover")
+        rowCells[i].classList.remove("hover-right")
       }
     }
   } else {
     let rowCells = document.querySelectorAll("p")
     for (let i =0; i< rowCells.length; i++){
-        rowCells[i].classList.remove("hover")
+        rowCells[i].classList.remove("hover-right")
     }
   }
 })
